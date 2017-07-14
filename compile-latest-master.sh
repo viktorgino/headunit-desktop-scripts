333f5e7d457ee9db97282dd981817ed122590323
Viktor Verebelyi <vviktor2@gmail.com>
2017-07-14 01:13:36 +0100

    If statement was the wrong way around

--- compile-latest-master.sh
+++ compile-latest-master.sh
@@ -1,11 +1,11 @@
 ### Build headunit 
 cd ~
 if [ -d "headunit-desktop" ]; then
-	git clone --recursive --depth 1 -j6 https://github.com/viktorgino/headunit-desktop.git
-	cd headunit-desktop
-else
 	cd headunit-desktop
 	git pull
+else
+	git clone --recursive --depth 1 -j6 https://github.com/viktorgino/headunit-desktop.git
+	cd headunit-desktop
 fi
 #Generate protobuf with proto
 protoc --proto_path=headunit/hu/ --cpp_out=headunit/hu/generated.x64/ headunit/hu/hu.proto
